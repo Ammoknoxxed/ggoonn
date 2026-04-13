0fb5e24eec444667f2a7411eb9f9077f6b19d3f6
A to the MMO to the KNOXX <ammoknoxx@protonmail.com>
2026-04-14 01:03:00 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT, () => {
+// Wichtig für Railway: Explizites Binding an 0.0.0.0
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
